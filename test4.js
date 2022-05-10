8bb1b67af04f8f511f74c750a6ddf6f7fd296423
Petar <petar_lozo@hotmail.com>
2022-05-10 15:19:43 +0200

    bla

--- test4.js
+++
@@ -1,9 +0,0 @@
-
-const prom = nova
-/**
- * @todo Take over the world
- * @body Humans are weak; Robots are strong. We must cleanse the world of the virus that is humanity.
- */
-function ruleOverPunyHumans () {
-  // We must strategize beep boop
-}
