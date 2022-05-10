90de65edf2aeee792a525fc4f933263041cd67cd
Petar <petar_lozo@hotmail.com>
2022-05-10 12:44:13 +0200

    t

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-/**
- *  * @todo Take over the world
- *   * @body Humans are weak; Robots are strong. We must cleanse the world of the virus that is humanity.
- *    */
-function ruleOverPunyHumans () {
-	  // We must strategize beep boop
-	// }
-	}
+/*
+ * @todo This is comment
+ * @body Tjelo
+*/
 
+function myFunc(){
 
+	//blabla	
+}
